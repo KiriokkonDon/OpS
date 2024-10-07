8556be0edd1bf0bdd65fdc59eaa45170bc24a712
KiriokkonDon <akane.vip@mail.ru>
2024-10-07 07:14:02 +0300

    add cmake

--- build.cmd
+++ build.cmd
@@ -1,4 +1,32 @@
 @echo off
-git pull
-mingw32-make clean-w
-mingw32-make
+
+for /f "tokens=*" %%i in ('git symbolic-ref --short HEAD') do set current_branch=%%i
+
+echo Текущая ветка: %current_branch%
+
+git pull origin %current_branch% --rebase
+
+IF %ERRORLEVEL% NEQ 0 (
+    echo Ошибка при выполнении git pull.
+    exit /b 1
+)
+
+if not exist build mkdir build
+cd build
+
+cmake ..
+
+IF %ERRORLEVEL% NEQ 0 (
+    echo Ошибка при конфигурации CMake.
+    exit /b 1
+)
+
+cmake --build .
+
+IF %ERRORLEVEL% NEQ 0 (
+    echo Ошибка сборки.
+    exit /b 1
+)
+
+echo Сборка завершена успешно!
+cd ..
--- build.sh
+++ build.sh
@@ -1,4 +1,36 @@
 #!/bin/bash
-git pull
-make clean-l
-make
+
+current_branch=$(git symbolic-ref --short HEAD)
+
+echo "Текущая ветка: $current_branch"
+
+git pull origin $current_branch --rebase
+
+if [ $? -ne 0 ]; then
+  echo "Ошибка при выполнении git pull. Проверьте конфликтующие изменения."
+  exit 1
+fi
+
+if [ ! -d "build" ]; then
+  mkdir build
+fi
+
+cd build
+
+cmake ..
+
+if [ $? -ne 0 ]; then
+  echo "Ошибка при конфигурации CMake."
+  exit 1
+fi
+
+cmake --build .
+
+if [ $? -eq 0 ]; then
+  echo "Сборка завершена успешно!"
+else
+  echo "Ошибка сборки."
+  exit 1
+fi
+
+cd ..
